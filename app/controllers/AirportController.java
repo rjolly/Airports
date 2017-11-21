64eda829eef069a7b1f5745a8ea3a861818b3e04
Raphael Jolly <rjolly@users.sourceforge.net>
2017-11-21 11:01:56 +0100

    Support for country code in lower case

--- app/controllers/AirportController.java
+++ app/controllers/AirportController.java
@@ -12,6 +12,7 @@ import models.Runway;
 
 import java.util.Map;
 import java.util.List;
+import java.util.Optional;
 import java.util.Collections;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
@@ -44,7 +45,7 @@ public class AirportController extends Controller {
 			return badRequest(views.html.listAirports.render(asScala(Collections.emptyList()), boundForm));
 		} else {
 			final AirportData data = boundForm.get();
-			final String countryCode = data.getCountryCode();
+			final String countryCode = Optional.ofNullable(data.getCountryCode()).map(c -> c.toUpperCase()).orElse(null);
 			final Map<Integer, List<Runway>> rba = airports.getRunwaysByAirport();
 			final Map<String, List<Airport>> abc = airports.getAirportsByCountry();
 			return ok(views.html.listAirports.render(asScala(abc.containsKey(countryCode)?abc.get(countryCode).stream().map(airport -> Pair.of(airport, rba.get(airport.id))).collect(Collectors.toList()):Collections.emptyList()), boundForm));
